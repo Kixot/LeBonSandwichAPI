e5255c09caac74596633ea1e3d5ab155e9869777
Kixot <seb.kixot@gmail.com>
2017-01-31 17:09:37 +0100

    fef

--- src/main/java/boundary/SandwichResource.java
+++ src/main/java/boundary/SandwichResource.java
@@ -1,25 +1,40 @@
 package boundary;
 
 import entity.Sandwich;
-
+import java.util.List;
+import java.util.UUID;
 import javax.ejb.Stateless;
+import javax.persistence.CacheStoreMode;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.util.List;
 
 @Stateless
 public class SandwichResource {
 
     @PersistenceContext
-    private EntityManager em;
+    EntityManager em;
+
 
     public Sandwich findById(String id){
-        return this.em.find(Sandwich.class, id);
+        return this.em.find(Sandwich.class,id);
     }
 
-    public List<Sandwich> findAll(String sandwichId) {
-        Query query = em.createNamedQuery("Sandwich.FindAll", Sandwich.class);
-        return query.getResultList();
+    public List<Sandwich> findAll() {
+        Query q = this.em.createNamedQuery("Sandwich.FindAll", Sandwich.class);
+        return q.getResultList();
+    }
+
+    public Sandwich save(Sandwich sdw) {
+        sdw.setId(UUID.randomUUID().toString());
+        return this.em.merge(sdw);
+    }
+
+    public void delete(String id) {
+        try {
+            Sandwich ref = this.em.getReference(Sandwich.class, id);
+            this.em.remove(ref);
+        } catch (EntityNotFoundException e) {}
     }
 }
